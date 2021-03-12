007ef7f79eafd19c9f1885f992bbc053009441aa
RaqueOrtegax <raquelortega@hotmail.nl>
2021-03-12 17:23:07 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    // This gets a "handle" to the clock div in our HTML
+    var clockDiv = document.getElementById('clock');
+
+    // Then we set the text inside the clock div 
+    // to the hours, minutes, and seconds of the current time
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+  }
+
+  // This runs the displayTime function the first time
+  displayTime();
+;
+
+setInterval(displayTime, 1000);
