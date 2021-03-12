ef1c0bef5eb9e206a0c96c5718e86d6d4f6203cd
RaqueOrtegax <raquelortega@hotmail.nl>
2021-03-12 17:42:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,17 @@ function displayTime() {
 
   // This runs the displayTime function the first time
   displayTime();
+  setInterval(displayTime, 1000);
 ;
 
-setInterval(displayTime, 1000);
+var t = new Date().getMinutes();
+// onder de 20 minuten heeft het een lichtblauwe kleur
+if (t < 20) {
+    document.body.style.backgroundColor="#80d4ea";
+} else if (t < 40) {
+	// onder de 40 minuten heeft het een donkerblauwe kleur
+    document.body.style.backgroundColor="darkblue";
+} else {
+	// Na de 40 minuten heeft het een zwarte kleur
+    document.body.style.backgroundColor="black";
+}
